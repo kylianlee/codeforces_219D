fe7fdfae5d757a0eac0531650d3ae48b27539d8f
Kylian <hsjgc1234@gmail.com>
2021-04-29 23:36:06 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/codeforces_219D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/codeforces_219D.dir/main.cpp.o"
+  "codeforces_219D"
+  "codeforces_219D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codeforces_219D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
